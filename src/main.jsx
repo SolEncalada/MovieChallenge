fa8f0541242b00fc18b80a3561a70b26defbc88c
Katherine Encalada <katherineencalada@Katherines-MacBook-Air.local>
2023-10-16 14:02:50 -0500

    init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import './main.css'
+
+/* ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+  </React.StrictMode>,
+)*/
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
