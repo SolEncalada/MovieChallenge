2961542b961bc1176a468d53ad3af4d8afe3e924
Katherine Encalada <katherineencalada@Katherines-MacBook-Air.local>
2023-10-16 21:55:44 -0500

    components/pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
-import './main.css'
+import React from "react";
+import {createRoot} from 'react-dom/client';
+//import ReactDOM from "react-dom";
+import { App } from "./App";
+import "./main.css";
 
 /* ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
   </React.StrictMode>,
 )*/
+const root = createRoot(document.getElementById('root'));
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+root.render(<App/>);
+//ReactDOM.render(<App />, document.getElementById("root"));
